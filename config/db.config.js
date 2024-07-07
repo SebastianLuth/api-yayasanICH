bc013734ab5decd76f0d1b7c19d58e29699dcf0e
Sebastian Luth Hasibuan <134922313+SebastianLuth@users.noreply.github.com>
2024-07-08 01:07:04 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://sebastianluthhasibuan:Sebas18112002@cluster0.ol8bcxm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    url: process.env.MONGODB_URI
 }
