7e0d5ee90f5b491ea44e90141da93e8fffad7957
Sebastian Luth Hasibuan <134922313+SebastianLuth@users.noreply.github.com>
2024-07-09 22:07:37 +0700

    Update upload.js

--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -1,37 +1,18 @@
 const multer = require('multer');
-const path = require('path');
+const cloudinary = require('cloudinary').v2; 
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-// Set storage engine
-const storage = multer.diskStorage({
-    destination: './uploads/pdf',
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'uploads/pdf', // Folder di Cloudinary
+        allowedFormats: 'pdf',
+        public_id: (req, file) => file.filename + '-' + Date.now(),
+    },
 });
 
-// Initialize upload variable
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 1000000 }, // 1MB limit
-    fileFilter: (req, file, cb) => {
-        checkFileType(file, cb);
-    }
+    limits: { fileSize: 3000000 }, // 3MB limit
 }).single('pdf');
-
-// Check file type
-function checkFileType(file, cb) {
-    // Allowed ext
-    const filetypes = /pdf/;
-    // Check ext
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    // Check mime
-    const mimetype = filetypes.test(file.mimetype);
-
-    if (mimetype && extname) {
-        return cb(null, true);
-    } else {
-        cb('Error: PDFs Only!');
-    }
-}
-
 module.exports = upload;
