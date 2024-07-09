a282d8df2fd296160d59a5fca73dc10904b20f8e
Sebastian Luth Hasibuan <134922313+SebastianLuth@users.noreply.github.com>
2024-07-10 04:59:44 +0700

    Update allowed_origins.js

--- config/allowed_origins.js
+++ config/allowed_origins.js
@@ -4,7 +4,8 @@ const origins = [
     'http://localhost:8080',
     'http://localhost:8081',
     'http://127.0.0.1:5173',
-    'https://yayasan-ich.vercel.app'
+    'https://yayasan-ich.vercel.app',
+    *
   ]
   
 module.exports = origins;
