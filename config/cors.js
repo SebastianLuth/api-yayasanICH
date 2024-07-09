83c59355659cef6f2aaa19f3b21fd913475bcf80
Sebastian Luth Hasibuan <134922313+SebastianLuth@users.noreply.github.com>
2024-07-10 02:30:07 +0700

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -1,17 +1,13 @@
 const allowedOrigins = require('./allowed_origins')
-
-
 const corsOptions = {
     origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
     },
     credentials: true, 
     optionsSuccessStatus: 200
-
-  };
-
+};
 module.exports = corsOptions
